b58f5cefe48bff09dab127960ca77f665da1fed0
Jeff Standen <jeff@webgroupmedia.com>
2020-01-21 13:25:42 -0800

    * [Automations/Datastores] Implemented a key/value datastore service for automations. This allows for shared keys (permitted by policies) and supports key expiration.

--- features/cerberusweb.core/patches/10.x/10.0.0.php
+++ features/cerberusweb.core/patches/10.x/10.0.0.php
@@ -283,6 +283,23 @@ if(!isset($tables['portal_widget'])) {
 }
 
 // ===========================================================================
+// Add `automation_datastore` table
+
+if(!isset($tables['automation_datastore'])) {
+	$sql = sprintf("
+		CREATE TABLE `automation_datastore` (
+		`data_key` varchar(255) NOT NULL DEFAULT '',
+		`data_value` mediumtext,
+		`expires_at` int(10) unsigned NOT NULL DEFAULT 0,
+		PRIMARY KEY (`data_key`)
+		) ENGINE=%s
+	", APP_DB_ENGINE);
+	$db->ExecuteMaster($sql) or die("[MySQL Error] " . $db->ErrorMsgMaster());
+
+	$tables['automation_datastore'] = 'automation_datastore';
+}
+
+// ===========================================================================
 // Finish up
 
 return TRUE;
